b2e286810d55a82eb746b29f87b6ebe87483b02a
Christian <cedenoc181@gmail.com>
2023-06-12 22:22:55 -0400

    added image folder and files folder

--- src/Other/OtherPDF.js
+++ src/Other/OtherPDF.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 function OtherPDF() {
   return (
-    <div>OtherPDF</div>
+    <div>
+
+
+<iframe 
+src= "https://docs.google.com/document/d/1UrjEBk2_uXxiw3BeYfcB9HNHQwwD7i58HvLw2x9u7to/edit?usp=sharing"
+height="450"
+width="400"
+style={{ border: "2px"}}
+title="MetroPlus"
+id="metroplus"
+></iframe>
+
+    </div>
   )
 }
 
