d0fdd60b6636f6646f6f39549d0cdbf2aae2ba99
Christian <cedenoc181@gmail.com>
2023-06-12 23:08:31 -0400

    added image and files folder for map

--- src/APOS/APOS.js
+++ src/APOS/APOS.js
@@ -3,6 +3,13 @@ import "./APOS.css";
 import APOSPDF from "./APOSPDF";
 
 function APOS() {
+
+  const AposImages = require.context("./APOS-Images", true);
+  const AposFiles = require.context("./APOS-files", true);
+
+  const imageList = AposImages.keys().map(image => AposImages(image));
+  const fileList = AposFiles.keys().map(file => AposFiles(file));
+
   return (
     <div className="apos bg-gray-100 w-fill h-screen" id="APOS">
     <div className="Prompt">
